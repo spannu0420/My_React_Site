33046ef802d3962620b237bc1e3f0e79005d412c
spannu0420 <spannu0420@gmail.com>
2026-06-12 15:30:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,78 +3,54 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Core Layout Components
-
 // Pages Components
-
-import NavScrollExample from './MDA/Navbar.jsx';
-import FluidExample from './MDA/Home.jsx';
-import About_us from './MDA/About_us.jsx';
-import Why_us from './MDA/Why_us.jsx';
-import Contact from './MDA/Contact.jsx';
-import Blog from './MDA/Blog.jsx';
-import Products from './MDA/Products.jsx';
-import Seed_Drill_Pro from './MDA/Category_1.jsx';
-import Cultivator_Shovels_Pro from './MDA/category_2.jsx';
-import Duck_Sweeps_Pro from './MDA/Category_5.jsx';
-import Tiller_Duck_Pro from './MDA/Category_4.jsx';
-import Footer from './MDA/Footer.jsx';
-import Inter_Shovels_Pro from './MDA/Category_3.jsx';
-import M_B_Plough_Pro from './MDA/M_B.jsx';
-import Rotary_Pro from './MDA/Rotary_Blades.jsx';
-import Rotavator_Pro from './MDA/Rotavator.jsx';
-import Plough from './MDA/Plough.jsx';
-import Sub_Soiler from './MDA/Sub_soiler_blades.jsx';
-import FloatingButtons from './MDA/Floting_Buttons.jsx';
-import Certificates from './MDA/Certificates.jsx';
-
-
-
+import NavScrollExample from './MDA/Navbar';
+import FluidExample from './MDA/Home';
+import About_us from './MDA/About_us';
+import Why_us from './MDA/Why_us';
+import Contact from './MDA/Contact';
+import Blog from './MDA/Blog';
+import Products from './MDA/Products';
+import Seed_Drill_Pro from './MDA/Category_1';
+import Cultivator_Shovels_Pro from './MDA/category_2';
+import Duck_Sweeps_Pro from './MDA/Caterory_5';
+import Tiller_Duck_Pro from './MDA/Category_4';
+import Footer from './MDA/Footer';
+import Inter_Shovels_Pro from './MDA/Category_3';
+import M_B_Plough_Pro from './MDA/M_B';
+import Rotary_Pro from './MDA/Rotary_Blades';
+import Rotavator_Pro from './MDA/Rotavator';
+import Plough from './MDA/Plough';
+import Sub_Soiler from './MDA/Sub_soiler_blades';
+import FloatingButtons from './MDA/Floting_Buttons';
+import Certificates from './MDA/Certificates';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Header/Navbar */}
-        <NavScrollExample />
-
-        {/* Dynamic Content Routes */}
-        <Routes>
-          {/* Default/Home Routes */}
-          <Route path="/" element={<FluidExample />} />
-          <Route path="/Home" element={<FluidExample /> } />
-          
-          {/* Main Pages */}
-          <Route path="/About_us" element={<About_us />} />
-          <Route path="/why_us" element={<Why_us />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/Products" element={<Products />} />
-
-          {/* Categories Pages */}
-          <Route path="/Seed_Drill_Pro" element={<Seed_Drill_Pro />} />
-          <Route path="/Cultivator_Shovels_Pro" element={<Cultivator_Shovels_Pro />} />
-          <Route path="/Inter_Shovels_Pro" element={<Inter_Shovels_Pro />} />
-          <Route path="/Tiller_Duck_Pro" element={<Tiller_Duck_Pro />} />
-          <Route path="/Duck_Sweeps_Pro" element={<Duck_Sweeps_Pro />} />
-          <Route path='/M_B_Plough_Pro' element={<M_B_Plough_Pro />} />
-          <Route path='/Rotary_Pro' element={<Rotary_Pro />} />
-          <Route path='/Rotavator_Pro' element={<Rotavator_Pro />} />
-          <Route path='/Plough' element={<Plough />} />
-          <Route path='/Sub_Soiler' element={<Sub_Soiler />} />
-          <Route path='/Certificates' element={<Certificates />} />
-
-
-
-
-
-
-          </Routes>
-
-          <FloatingButtons />
-
-        {/* Footer */}
-        <Footer/>
-      </div>
+      {/* Floating buttons standard syntax */}
+      <FloatingButtons />
+      <NavScrollExample />
+      <Routes>
+        <Route path="/" element={<FluidExample />} />
+        <Route path="/about" element={<About_us />} />
+        <Route path="/why-choose-us" element={<Why_us />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/seed-drill-parts" element={<Seed_Drill_Pro />} />
+        <Route path="/cultivator-shovels" element={<Cultivator_Shovels_Pro />} />
+        <Route path="/duck-foot-sweeps" element={<Duck_Sweeps_Pro />} />
+        <Route path="/tiller-duck-foot" element={<Tiller_Duck_Pro />} />
+        <Route path="/inter-cultivator-shovels" element={<Inter_Shovels_Pro />} />
+        <Route path="/mb-plough-shear" element={<M_B_Plough_Pro />} />
+        <Route path="/rotary-tiller-blades" element={<Rotary_Pro />} />
+        <Route path="/rotavator-parts" element={<Rotavator_Pro />} />
+        <Route path="/plough-parts" element={<Plough />} />
+        <Route path="/sub-soiler-blades" element={<Sub_Soiler />} />
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
